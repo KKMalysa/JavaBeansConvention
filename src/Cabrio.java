617067cae4199864f90df967034aaa85ee5b0e48
karol <karol.malysa@interia.pl>
2021-04-02 13:40:22 +0200

    problem

---
+++ src/Cabrio.java
@@ -0,0 +1,49 @@
+public class Cabrio {
+    private String name;
+    private String brand;
+    private boolean roofOpen;
+    private boolean isMoving;
+
+    public Cabrio(String name, String brand) {
+        this.name = name;
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public void setMoving(boolean isMoving) {
+        this.isMoving = isMoving;
+    }
+
+    public boolean isRoofOpen() {
+        return roofOpen;
+    }
+
+    public void setRoofOpen(boolean roofOpen) {
+        if(isMoving = false){
+            this.roofOpen = roofOpen;
+        }else {
+            System.out.println(" You can't open/close roof while moving");
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Cabrio cabrio = new Cabrio("Vento", "Vw");
+
+
+        System.out.println("status wyjściowy");
+        System.out.printf("Car status.\n Is moving: %s\n Is roof Open: %s", cabrio.isMoving(), cabrio.isRoofOpen());
+        System.out.println();
+        System.out.println("auto stoi - próbuje otworzyć dach");
+        cabrio.setRoofOpen(true);
+
+//        cabrio.setMoving(true);
+//        cabrio.setRoofOpen(false);
+//        System.out.printf("Car status.\n Is moving: %s\n Is roof Open: %s", cabrio.isMoving(), cabrio.isRoofOpen());
+//        System.out.println();
+//        System.out.println();
+//        cabrio.setMoving(false);
+//        cabrio.setRoofOpen(false);
+//        System.out.printf("Car status.\n Is moving: %s\n Is roof Open: %s", cabrio.isMoving(), cabrio.isRoofOpen());
+
+    }
 }
